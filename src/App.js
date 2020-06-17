1a74207453fbabb435e15fadd59a6375b2809ec7
mianihsanshah <mianihsanshah@gmail.com>
2020-06-17 19:00:10 +0500

    react project created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> Boot Camp Class#2 Assignment</h1>
+      <h4>1:  React Project Created using <u>create-react-app command</u></h4>
+      <h4>2:  Open the created project in VS Code and edit it using command <u>code .</u></h4>
+      <h4>3:  Give following commands to view app in browser: <u>cd react-base-app, npm start</u></h4>
+      <h4>4:  Added the React Project to GitHub using <u>CLI</u> </h4>
+      <h4>5.1: <u>GitHub Actions Workflow</u> of deploying a the React App to Surge Created</h4>
+      <h4>5.2:  <u>bug</u> in the workflow removed and its working now</h4>
+      <h4>6:  React App is running <u>locally and remotely</u> on the surge, start learning React</h4>  
+      <h4>7:  My <u>GitHub repo UTL and surge website URL</u> reported into panacloud git issues</h4>
     </div>
   );
 }
